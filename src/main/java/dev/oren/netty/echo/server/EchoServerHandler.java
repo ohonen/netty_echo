1b324703433ee46d1e5c7ae599d1afe46ddfdb4d
Oren Honen <oren.honen@gmail.com>
2017-11-17 10:02:45 +0200

    Initial commit

---
+++ src/main/java/dev/oren/netty/echo/server/EchoServerHandler.java
@@ -0,0 +1,9 @@
+package dev.oren.netty.echo.server;
+
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * @author Oren Honen
+ */
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+}
